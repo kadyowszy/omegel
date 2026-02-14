064a89deeca08b662826d74cb31c21949a8a67f6
kadyowszy <samuellance.panganiban@cvsu.edu.ph>
2026-02-14 20:32:12 +0800

    Refactor video streaming and event handling

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io(); 
+const socket = io();
 const myVideo = document.getElementById('myVideo');
 const partnerVideo = document.getElementById('partnerVideo');
 const startBtn = document.getElementById('startBtn');
@@ -6,100 +6,53 @@ const startBtn = document.getElementById('startBtn');
 let myStream;
 let peer;
 
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-}).then(stream => {
-    myStream = stream;
-    addVideoStream(myVideo, stream);
-}).catch(err => {
-    console.error("Error accessing media devices.", err);
-    alert("Please allow camera access!");
-});
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then(stream => {
+        myStream = stream;
+        myVideo.srcObject = stream;
+        myVideo.play();
+    })
+    .catch(err => alert("Camera blocked! Please allow access."));
 
 startBtn.addEventListener('click', () => {
-    const status = startBtn.innerText;
-
-    if (status === "Find Stranger" || status === "Next Stranger") {
+    if (startBtn.innerText === "Find Stranger" || startBtn.innerText === "Next Stranger") {
         startSearch();
-    } else if (status === "Leave") {
-        skipPartner();
+    } else {
+        location.reload();
     }
 });
 
 function startSearch() {
+    console.log("Button clicked. Joining queue...");
     startBtn.innerText = "Searching...";
     startBtn.disabled = true;
     socket.emit('join_queue');
 }
 
-function skipPartner() {
-    if (peer) {
-        peer.destroy();
-        peer = null;
-    }
-    partnerVideo.srcObject = null;
-    
-    startSearch();
-}
-
 socket.on('match_found', (data) => {
-    startBtn.disabled = false;
+    console.log("Match found! Initiator: " + data.initiator);
     startBtn.innerText = "Leave";
-    
-    const partnerId = data.partnerId;
-    const initiator = data.initiator;
+    startBtn.disabled = false;
 
     peer = new SimplePeer({
-        initiator: initiator,
+        initiator: data.initiator,
         stream: myStream,
         trickle: false
     });
 
     peer.on('signal', (signal) => {
-        socket.emit('signal', {
-            target: partnerId,
-            signal: signal
-        });
+        socket.emit('signal', { target: data.partnerId, signal: signal });
     });
 
     peer.on('stream', (stream) => {
+        console.log("Received partner stream");
         partnerVideo.srcObject = stream;
         partnerVideo.play();
     });
-
-    peer.on('close', () => {
-        handlePartnerDisconnect();
-    });
     
-    peer.on('error', (err) => {
-        console.log("Peer error:", err);
-        handlePartnerDisconnect();
-    });
-
     window.peer = peer;
 });
 
 socket.on('signal', (data) => {
-    if (peer) {
-        peer.signal(data.signal);
-    }
+    if (peer) peer.signal(data.signal);
 });
-
-function handlePartnerDisconnect() {
-    if (peer) {
-        peer.destroy();
-        peer = null;
-    }
-    partnerVideo.srcObject = null;
-    startBtn.innerText = "Next Stranger";
-    startBtn.disabled = false;
-    alert("Stranger disconnected.");
-}
-
-function addVideoStream(video, stream) {
-    video.srcObject = stream;
-    video.addEventListener('loadedmetadata', () => {
-        video.play();
-    });
-}
